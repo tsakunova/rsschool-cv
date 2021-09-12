8df20a8a5647f045ea50eefdbea42ef16acf3064
tsakunova <tv.tsakunova@gmail.com>
2021-09-12 18:35:43 +0300

    feat: burger-menu

---
+++ script.js
@@ -0,0 +1,10 @@
+let burgerButton = document.querySelector('.burger-button');
+let burgerMenu = document.querySelector('.burger-menu')
+let burgerMenuList = document.querySelector('.burger-menu-list')
+burgerButton.addEventListener('click',function(){
+  burgerMenu.classList.toggle("burger-menu_active");
+});
+burgerMenuList.addEventListener('click',function(){
+  burgerMenu.classList.toggle("burger-menu_active");
+  
+});
